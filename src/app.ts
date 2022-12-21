e9d0a00e4496839b210046ef0a8029c5c8d75aaa
Leonardo <yleeoembd@gmail.com>
2022-12-21 09:42:06 -0300

    fix: cors para acessar o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
-import express from 'express'
-import 'express-async-errors';
-import { handleErrorMiddleware } from './middlewares/handleError.middleware';
-import announcementsRoutes from './routes/announcements.routes';
-import usersRoutes from './routes/users.routes';
-import SwaggerUi from 'swagger-ui-express';
-import swaggerDocs from '../swagger.json'
+import express from "express";
+import "express-async-errors";
+import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
+import announcementsRoutes from "./routes/announcements.routes";
+import usersRoutes from "./routes/users.routes";
+import SwaggerUi from "swagger-ui-express";
+import swaggerDocs from "../swagger.json";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use("/users", usersRoutes)
-app.use("/announcements", announcementsRoutes)
+app.use("/users", usersRoutes);
+app.use("/announcements", announcementsRoutes);
 
-app.use('/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocs))
-app.use(handleErrorMiddleware)
+app.use("/docs", SwaggerUi.serve, SwaggerUi.setup(swaggerDocs));
+app.use(handleErrorMiddleware);
 
-export default app
+export default app;
