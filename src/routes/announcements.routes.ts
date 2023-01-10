0ebd16ded17f616a438dfbf0104fdcf0c5f26dd8
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-01-10 18:01:35 -0300

    feat: colocando middlewares

--- src/routes/announcements.routes.ts
+++ src/routes/announcements.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import {announcementsGetIdController,announcementsGetController, announcementsPostController,announcementsGetCommentsByIDController, updateAnnouncementsController} from "../controllers/announcements.controllers";
 import { postReviewController } from "../controllers/reviws.controller";
+import verifyAdmOrOwnerMiddleware from "../middlewares/verifyAdmOrOwner.middleware";
 import verifyAuthMiddleware from "../middlewares/verifyAuth.middleware";
 
 const announcementsRoutes = Router();
 
 announcementsRoutes.get("", announcementsGetController)
 announcementsRoutes.get("/:id", announcementsGetIdController)
-announcementsRoutes.patch("/:id", updateAnnouncementsController )
+announcementsRoutes.patch("/:id", verifyAuthMiddleware, verifyAdmOrOwnerMiddleware, updateAnnouncementsController )
 //verifyAdmOrOwnerMiddleware
 announcementsRoutes.get("/:id/comments", announcementsGetCommentsByIDController)
 announcementsRoutes.post("", verifyAuthMiddleware, announcementsPostController)
