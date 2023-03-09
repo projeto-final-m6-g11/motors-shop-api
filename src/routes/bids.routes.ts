630fac28d9cf4275f7515c769ab19986e66fa334
mateus <mateusbona28@gmail.com>
2023-03-09 10:10:06 -0300

    ajustando rota de lances para dentro de anúncios

--- src/routes/bids.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { postBidController } from "../controllers/bids.controllers";
-
-const bidRoutes = Router()
-
-bidRoutes.post("", postBidController)
-
-export default bidRoutes
