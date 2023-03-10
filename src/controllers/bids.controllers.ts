60d35d7b74cad6a6a4d7b7da9f6c6767fb6e083d
mateus <mateusbona28@gmail.com>
2023-03-10 10:13:55 -0300

    feat: rota para deleção de lance criada, finalizada e testada

--- src/controllers/bids.controllers.ts
+++ src/controllers/bids.controllers.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
-import { createNewBid } from "../services/bids.services";
+import { createNewBid, deleteBidService } from "../services/bids.services";
 
 export const postBidController = async (request: Request, response: Response) => {
     
     const newBid = await createNewBid(request.body, request.user, request.params.id)
 
     return response.status(201).json(newBid)
+}
+
+export const deleteBidController = async (request: Request, response: Response) => {
+
+    await deleteBidService(request.params.id, request.params.bidId, request.user)
+
+    return response.status(204).json({})
 }
--- src/services/bids.services.ts
+++ src/services/bids.services.ts
@@ -1,6 +1,7 @@
 import AppDataSource from "../data-source";
 import { Announcement } from "../entities/announcement.entity";
 import { Bid } from "../entities/bid.entity";
+import { User } from "../entities/user.entity";
 import { AppError } from "../errors/AppError";
 import { IBid, IBidPostRequest } from "../interfaces/bid.interfaces";
 import { ITokenUserInfo } from "../interfaces/users.interfaces";
@@ -9,18 +10,21 @@ export const createNewBid = async (requestBid: IBidPostRequest, tokenUserInfo: I
 
     const announcementsRepository = AppDataSource.getRepository(Announcement)
     const bidsRepository = AppDataSource.getRepository(Bid)
+    const usersRepository = AppDataSource.getRepository(User)
 
     if(!announcementId) {
         throw new AppError("invalid announcement id", 403);
     }
 
     const announcement: any = await announcementsRepository.findOneBy({ id: announcementId })
+    const user: any = await usersRepository.findOneBy({ id: tokenUserInfo.id })
 
     if ( announcement.bids.length === 0 ) {
 
         const newBid = {
             ...requestBid,
-            announcement
+            announcement,
+            user
         }
 
         bidsRepository.create(newBid)
@@ -28,8 +32,6 @@ export const createNewBid = async (requestBid: IBidPostRequest, tokenUserInfo: I
 
         return createdBid
     }
-
-    console.log(announcement.bids)
 
     for (let i = 0; i < announcement.bids.length; i++) {
         const bid = announcement.bids[i]
@@ -40,10 +42,11 @@ export const createNewBid = async (requestBid: IBidPostRequest, tokenUserInfo: I
             throw new AppError("you cannot bid less than or equal to the highest bid", 403);
         }
 
-        if (i + 1 === announcement.bids.length || canCreateBid) {
+        if (i + 1 === announcement.bids.length && canCreateBid) {
             const newBid = {
                 ...requestBid,
-                announcement
+                announcement,
+                user
             }
     
             bidsRepository.create(newBid)
@@ -53,4 +56,31 @@ export const createNewBid = async (requestBid: IBidPostRequest, tokenUserInfo: I
         }
     }
 
+}
+
+
+export const deleteBidService = async (announcementId: string, bidId: string, tokenUserInfo: ITokenUserInfo) => {
+
+    const usersRepository = AppDataSource.getRepository(User)
+    const bidsRepository = AppDataSource.getRepository(Bid)
+
+    const user: any = await usersRepository.findOne({ where: {
+        id: tokenUserInfo.id
+    }, relations: {
+        bids: true
+    } })
+
+    for (let i = 0; i < user.bids.length; i++) {
+        const bid = user.bids[i]
+        let deleted = false
+
+        if (bid.id === bidId) {
+            deleted = true
+            await bidsRepository.delete(bidId)
+        }
+
+        if (i + 1 === user.bids.length && !deleted) {
+            throw new AppError("you do not have permission to perform this action", 401);
+        }
+    }
 }
